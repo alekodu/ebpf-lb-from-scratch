3970df594c41d453dea3d89c87a373e298766138
Aleksandra Obeso Duque <ezobeal@ct-snic1.rnd.ki.sw.ericsson.se>
2023-06-08 15:14:34 +0200

    cleaning lb example

--- Makefile
+++ Makefile
@@ -1,25 +1,16 @@
-# TARGET = xdp_lb
-# TARGET = packetdrop
-TARGET = xdp_liz
+TARGET = xdp_lb
+XDP_MODE = xdpgeneric # xdpdrv, xdpoffload
+DEV = eth0 # enp130s0np0, bond0
 
-# For xdp_liz, make and also make user. The others don't have userspace programs
-
-USER_TARGET = ${TARGET:=_user}
 BPF_TARGET = ${TARGET:=_kern}
 BPF_C = ${BPF_TARGET:=.c}
 BPF_OBJ = ${BPF_C:.c=.o}
 
 xdp: $(BPF_OBJ)
-	bpftool net detach xdpgeneric dev eth0
+	bpftool net detach $(XDP_MODE) dev $(DEV)
 	rm -f /sys/fs/bpf/$(TARGET)
 	bpftool prog load $(BPF_OBJ) /sys/fs/bpf/$(TARGET)
-	bpftool net attach xdpgeneric pinned /sys/fs/bpf/$(TARGET) dev eth0 
-
-user: $(USER_TARGET)
-
-$(USER_TARGET): %: %.c  
-	gcc -Wall $(CFLAGS) -Ilibbpf/src -Ilibbpf/src/include/uapi -Llibbpf/src -o $@  \
-	 $< -l:libbpf.a -lelf -lz
+	bpftool net attach $(XDP_MODE) pinned /sys/fs/bpf/$(TARGET) dev $(DEV)
 
 $(BPF_OBJ): %.o: %.c
 	clang -S \
@@ -35,11 +26,7 @@ $(BPF_OBJ): %.o: %.c
 	llc -march=bpf -filetype=obj -o $@ ${@:.o=.ll}
 
 clean:
-	bpftool net detach xdpgeneric dev eth0
+	bpftool net detach $(XDP_MODE) dev $(DEV)
 	rm -f /sys/fs/bpf/$(TARGET)
 	rm $(BPF_OBJ)
 	rm ${BPF_OBJ:.o=.ll}
-
-
-
-
--- xdp_lb_kern.c
+++ xdp_lb_kern.c
@@ -1,15 +1,17 @@
 #include "xdp_lb_kern.h"
 
+// Hard codes containers IP addresses
 #define IP_ADDRESS(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
 
-#define BACKEND_A 2
-#define BACKEND_B 3
-#define CLIENT 4
-#define LB 5
+#define BACKEND_A 4
+#define BACKEND_B 5
+#define CLIENT 6
+#define LB 3
 
 SEC("xdp_lb")
 int xdp_load_balancer(struct xdp_md *ctx)
 {
+    // Locates the ethernet header and the IP header in the packet (context data)
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
 
@@ -19,6 +21,7 @@ int xdp_load_balancer(struct xdp_md *ctx)
     if (data + sizeof(struct ethhdr) > data_end)
         return XDP_ABORTED;
 
+    // Filter to only process IPv4 packets
     if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
         return XDP_PASS;
 
@@ -26,28 +29,36 @@ int xdp_load_balancer(struct xdp_md *ctx)
     if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end)
         return XDP_ABORTED;
 
+    // Filter to only process TCP packets
     if (iph->protocol != IPPROTO_TCP)
         return XDP_PASS;
 
     bpf_printk("Got TCP packet from %x", iph->saddr);
 
+    // Checks that the packet comes from the client (a client request)
     if (iph->saddr == IP_ADDRESS(CLIENT))
     {
+        // Implements a pseudo-random load balancing algorithm
         char be = BACKEND_A;
         if (bpf_ktime_get_ns() % 2)
             be = BACKEND_B;
 
+        // Updates destination IP and MAC addresses based on the chosen backend (a request)
         iph->daddr = IP_ADDRESS(be);
         eth->h_dest[5] = be;
-    }
+    } // Otherwise, packet comes from the backend (a backend response)
     else
     {
+        // Updates destination IP and MAC addresses to the one of the client (a response)
         iph->daddr = IP_ADDRESS(CLIENT);
         eth->h_dest[5] = CLIENT;
     }
+
+    // Updates source IP and MAC addresses to the one of the load balancer (for both requests and responses)
     iph->saddr = IP_ADDRESS(LB);
     eth->h_source[5] = LB;
 
+    // Recalculates and replaces IP header checksum (since they have been updated)
     iph->check = iph_csum(iph);
 
     return XDP_TX;
